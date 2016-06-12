3c6fbc41ddebbf5f1a9f584547c107bb22b428f5
aaronst <aaron@icebrg.io>
2016-06-12 00:03:50 -0700

    add ctypescrypto to setup

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ install_requires = [
     # List your project dependencies here.
     # For more details, see:
     # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
+    'ctypescrypto'
 ]
 
 
